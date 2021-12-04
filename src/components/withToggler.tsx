2590c5ef945a18504bf17ac7d4a01a92ad6e30c2
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-04 13:18:30 +0000

    Theme setup

--- src/components/withToggler.tsx
+++ src/components/withToggler.tsx
@@ -1,11 +1,17 @@
 import React, { useState, ComponentType } from 'react'
 
 export function withToggler<T>(Component: ComponentType<T>) {
-   return (hocProps: T) => {
+   return (hocProps: Omit<T, 'isToggled' | 'toggle'>) => {
       const [isToggled, setIsToggled] = useState(false)
       const toggle = () => setIsToggled(!isToggled)
 
-      return <Component isToggled={isToggled} toggle={toggle} {...hocProps} />
+      return (
+         <Component
+            isToggled={isToggled}
+            toggle={toggle}
+            {...(hocProps as T)}
+         />
+      )
    }
 }
 
