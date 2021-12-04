989f8ab8727cc778d4ef0024976c2ef88f08e890
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-04 10:35:04 +0000

    Theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react'
 import GlobalStyles from './styles/Global.styled'
-import { AppContainer, ModalContainer } from './containers/Container.styled'
+import { AppContainer } from './containers/Container.styled'
+import Modal from './components/Modal'
 
 const App: FC = () => {
    return (
       <>
          <GlobalStyles />
          <AppContainer>
-            <ModalContainer />
+            <Modal />
          </AppContainer>
       </>
    )
---
+++ src/components/Modal.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import { ModalContainer } from '../containers/Container.styled'
+
+const Modal: FC = () => {
+   return <ModalContainer></ModalContainer>
+}
+
+export default Modal
