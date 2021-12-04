4b91b82cb09fc260b3cf1f019e7162ef6dba4a6d
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-04 15:58:25 +0000

    Modal ip

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,1 +1,13 @@
-import React from 'react'
+import React, { createContext, ReactNode } from 'react'
+
+interface ContextInterface {
+   test: string
+}
+
+export const Context = createContext<ContextInterface | null>(null)
+
+export const ContextProvider = (children: ReactNode) => {
+   return (
+      <Context.Provider value={{ test: 'test' }}>{children}</Context.Provider>
+   )
+}
