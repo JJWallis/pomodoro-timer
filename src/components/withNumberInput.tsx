c85f43256b9454b42c7b6615d11c98bfb5ade351
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 09:50:08 +0000

    Modal timers ip

--- src/components/withNumberInput.tsx
+++ src/components/withNumberInput.tsx
@@ -3,12 +3,14 @@ import React, { ComponentType, useState } from 'react'
 export function withNumberInput<T>(Component: ComponentType<T>) {
    return (props: Omit<T, 'state' | 'updateState'>) => {
       const [numberState, setNumberState] = useState(0)
+      const updateState = (newNumber: string) =>
+         setNumberState(Number(newNumber))
 
       return (
          <Component
             {...(props as T)}
             state={numberState}
-            updateState={setNumberState}
+            updateState={updateState}
          />
       )
    }
