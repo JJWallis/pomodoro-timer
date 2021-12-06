dfe86228b5cb53d8f00a338d7072eb803ad4fe43
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 09:36:46 +0000

    Modal timers ip

--- src/components/ModalTimers.tsx
+++ src/components/ModalTimers.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react'
 import ModalSection from './ModalSection'
+import ModalTimer from './ModalTimer'
 
 const ModalTimers: FC = () => {
    return (
       <ModalSection>
          <h3>Time (MINUTES)</h3>
+         <ModalTimer label={'pomodoro'} />
       </ModalSection>
    )
 }
