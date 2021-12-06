1547c37a3bfd251c6acbe9320d2766a0858f6874
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 14:55:27 +0000

    Modal timers ip

--- src/components/ModalColors.tsx
+++ src/components/ModalColors.tsx
@@ -3,13 +3,18 @@ import ModalSection from './ModalSection'
 import RadioButton from './RadioButton'
 
 const ModalColors: FC = () => {
-   return (
-      <ModalSection heading="Color">
-         <RadioButton id="theme-red" defaultChecked={true} />
-         <RadioButton id="theme-blue" />
-         <RadioButton id="theme-purple" />
-      </ModalSection>
-   )
+   const produceRadioButtons = () => {
+      const themes = ['theme-red', 'theme-blue', 'theme-purple']
+      return themes.map((color) => (
+         <RadioButton
+            key={color}
+            id={color}
+            defaultChecked={themes.indexOf(color) === 0 ? true : false}
+         />
+      ))
+   }
+
+   return <ModalSection heading="Color">{produceRadioButtons()}</ModalSection>
 }
 
 export default ModalColors
