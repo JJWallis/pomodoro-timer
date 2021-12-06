f176797d49669acb61a7bc4cb925f96b45a3aa27
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 12:55:20 +0000

    Modal timers ip

--- src/components/ModalColors.tsx
+++ src/components/ModalColors.tsx
@@ -1,8 +1,15 @@
 import React, { FC } from 'react'
 import ModalSection from './ModalSection'
+import RadioButton from './RadioButton'
 
 const ModalColors: FC = () => {
-   return <ModalSection heading="Color"></ModalSection>
+   return (
+      <ModalSection heading="Color">
+         <RadioButton id="theme-red" />
+         <RadioButton id="theme-blue" />
+         <RadioButton id="theme-purple" />
+      </ModalSection>
+   )
 }
 
 export default ModalColors
