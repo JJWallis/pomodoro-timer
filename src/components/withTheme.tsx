53931f9b83f69045ea9a39eff30a8b3f7012b7b1
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 15:44:09 +0000

    Modal timers ip

--- src/components/withTheme.tsx
+++ src/components/withTheme.tsx
@@ -1,7 +1,17 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, ComponentType } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from '../styles/Theme'
 
-export const withTheme = ({ children }: { children: ReactNode }) => {
-   return <ThemeProvider theme={Theme}>{children}</ThemeProvider>
+// export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+//    return <ThemeProvider theme={Theme}>{children}</ThemeProvider>
+// }
+
+type withThemeType = <T>(Component: ComponentType<T>) => ComponentType<T>
+
+export const withTheme: withThemeType = (Component) => (props) => {
+   return (
+      <ThemeProvider theme={Theme}>
+         <Component {...props} />
+      </ThemeProvider>
+   )
 }
