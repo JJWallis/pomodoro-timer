004bf37c50125021eb8f8d4931010a41d6f146be
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 09:44:04 +0000

    Modal timers ip

--- src/components/withNumberInput.tsx
+++ src/components/withNumberInput.tsx
@@ -1,1 +1,15 @@
-import React from 'react'
+import React, { ComponentType, useState } from 'react'
+
+const withNumberInput = (Component: ComponentType) => {
+   return (props: any) => {
+      const [numberState, setNumberState] = useState(0)
+
+      return (
+         <Component
+            {...props}
+            state={numberState}
+            updateState={setNumberState}
+         />
+      )
+   }
+}
