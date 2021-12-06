50f85486f1f332a13f911fe5da9197ecd55980a5
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-06 12:58:56 +0000

    Modal timers ip

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -10,3 +10,5 @@ export const Theme = {
 // use HOC in this file to render ThemeProvider + pass value obj above to it
 // withTheme(Theme) - wrap App in index.tsx or in App.tsx
 // hook to use?
+// dynamic helper hook - check if data truthy + returns if so (ts destructing issue)
+// use TS generics to help us (vs any)
