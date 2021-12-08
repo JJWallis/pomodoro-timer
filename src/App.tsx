9d6142dd42023c1cbf14bee480510f4eff4cf6f0
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-08 16:43:58 +0000

    Reducer ip

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,40 @@ import React, { FC, useReducer } from 'react'
 import { AppContainer } from './containers/Container.styled'
 import Modal from './components/Modal'
 
-const reducer = (state, action) => {
-   switch (action.type) {
-      case 'deposit':
-         return state + action.payload
-      case 'withdraw':
-         return state - action.payload
+type TimerState = number
+type Actions =
+   | {
+        type: 'INCREMENT'
+        payload: number
+     }
+   | {
+        type: 'DECREMENT'
+        payload: number
+     }
+
+const timerReducer = (state: TimerState, action: Actions) => {
+   const { type, payload } = action
+   switch (type) {
+      case 'INCREMENT':
+         return state + payload
+      case 'DECREMENT':
+         return state - payload
       default:
          return state
    }
 }
 
 const App: FC = () => {
-   const deposit = (amount: number) => {
-      // if (amount.isFinite()) validation if desired
-      dispatch({ type: 'deposit', payload: amount })
-   }
-
-   const [amount, dispatch] = useReducer(reducer, 500)
-
+   const [amount, dispatch] = useReducer(timerReducer, 500)
    return (
       <>
          <AppContainer>
             <Modal />
-            <button onClick={() => deposit(500)}></button>
+            <button onClick={() => dispatch({ type: 'INCREMENT', payload: 1 })}>
+               Increment
+            </button>
+            <button>Decrement</button>
+            <div>{amount}</div>
          </AppContainer>
       </>
    )
