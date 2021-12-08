3645b3d502c5271abd4962701d5eb82d16aeac17
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-08 17:12:21 +0000

    Reducer ip

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,12 @@
-import React, { FC, useReducer } from 'react'
+import React, { FC } from 'react'
 import { AppContainer } from './containers/Container.styled'
 import Modal from './components/Modal'
 
-type ReducerState = number
-type ReducerActions =
-   | {
-        type: 'INCREMENT'
-        payload: number
-     }
-   | {
-        type: 'DECREMENT'
-        payload: number
-     }
-
-const timerReducer = (state: ReducerState, action: ReducerActions) => {
-   const { type, payload } = action
-   switch (type) {
-      case 'INCREMENT':
-         return state + payload
-      case 'DECREMENT':
-         return state - payload
-      default:
-         return state
-   }
-}
-
 const App: FC = () => {
-   const [amount, dispatch] = useReducer(timerReducer, 500)
-
    return (
       <>
          <AppContainer>
             <Modal />
-            <button onClick={() => dispatch({ type: 'INCREMENT', payload: 1 })}>
-               Increment
-            </button>
-            <button onClick={() => dispatch({ type: 'DECREMENT', payload: 1 })}>
-               Decrement
-            </button>
-            <div>{amount}</div>
          </AppContainer>
       </>
    )
