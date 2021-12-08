eaa0946591bf885864d4f65cef691aa8b009c4c9
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-08 17:22:07 +0000

    Theme ip

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React, { FC } from 'react'
 import { AppContainer } from './containers/Container.styled'
 import Modal from './components/Modal'
 
-const App: FC = () => {
+interface Props {
+   updateTheme: (newTheme: string) => void
+}
+
+const App: FC<Props> = ({ updateTheme }) => {
+   // pass down update theme method via context (passed into here via props + pass into HOC via argument)
+   // AppContext - passes down value obj in state (includes updateTheme method)
    return (
       <>
          <AppContainer>
