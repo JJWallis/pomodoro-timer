067b01afc735638b0fdacdc5ed8de854759d1c7f
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-10 14:20:52 +0000

    Theme ip

--- src/components/withTheme.tsx
+++ src/components/withTheme.tsx
@@ -1,11 +1,13 @@
 import React, { FC, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { Theme } from '../styles/Theme'
+import { Theme, Fonts } from '../styles/Theme'
 
 const { redTheme, blueTheme, purpleTheme } = Theme
+const { ffNoto, ffRoboto, ffRobotoMono } = Fonts
 
 export const ThemeContext: FC = ({ children }) => {
    const [currentTheme, setCurrentTheme] = useState(redTheme)
+   const [currentFont, setCurrentFont] = useState(ffNoto)
 
    const updateTheme = (newTheme: string) => {
       if (newTheme === 'theme-red') {
@@ -17,6 +19,16 @@ export const ThemeContext: FC = ({ children }) => {
       }
       // regex look ahead to split id at capital letter + toLowerCase + replace space with '-'
       // refactor useTheme into Context folder
+   }
+
+   const updateFont = (newFont: string) => {
+      if (newFont === 'font-noto') {
+         setCurrentFont(ffNoto)
+      } else if (newFont === 'font-roboto') {
+         setCurrentFont(ffRoboto)
+      } else {
+         setCurrentFont(ffRobotoMono)
+      }
    }
 
    return (
