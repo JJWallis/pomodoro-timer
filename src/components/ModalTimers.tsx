15d55d33979fa30de1c01b3717cfd1cdba65b002
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-13 19:11:03 +0000

    README updated

--- src/components/ModalTimers.tsx
+++ src/components/ModalTimers.tsx
@@ -1,11 +1,8 @@
 import React, { FC } from 'react'
 import ModalSection from './ModalSection'
 import ModalTimer from './ModalTimer'
-import { useLocalStorage } from '../hooks/useLocalStorage'
 
 const ModalTimers: FC = () => {
-   const [time, setTime] = useLocalStorage('timer', '')
-
    const produceTimerInputs = () => {
       return ['pomodoro', 'short-break', 'long-break'].map((label) => (
          <ModalTimer key={label} label={label} />
@@ -15,7 +12,6 @@ const ModalTimers: FC = () => {
    return (
       <ModalSection heading="Time (MINUTES)">
          {produceTimerInputs()}
-         <input value={time} onChange={(e) => setTime(e.target.value)} />
       </ModalSection>
    )
 }
--- src/hooks/useRadioButton.tsx
+++ src/hooks/useRadioButton.tsx
@@ -12,7 +12,6 @@ interface Themes {
 export function useRadioButton(updateTheme: Update, themes: Themes) {
    const { pm, sd, tr } = themes
    const [selectedOption, setSelectedOption] = useState(pm)
-   // useLocalStorage()
 
    return [pm, sd, tr].map((theme) => (
       <RadioButton
