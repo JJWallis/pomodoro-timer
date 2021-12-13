504a85424539ab4cde6cbfebb7e0cbbb795f2d37
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-13 17:58:06 +0000

    README updated

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 
-function getSavedValue(key: string, initialValue: string) {}
+function getSavedValue(key: string, initialValue: string): string | undefined {
+   const savedValue = localStorage.getItem(key)
+   return savedValue ? JSON.parse(savedValue) : initialValue
+}
 
 export function useLocalStorage(key: string, initialValue: string) {
    const [value, setValue] = useState(initialValue)
+   const savedValue = getSavedValue(key, initialValue)
 
    return [value, setValue]
 }
