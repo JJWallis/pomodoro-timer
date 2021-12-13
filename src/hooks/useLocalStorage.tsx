fa57be5f97782e1b3c3ba8da067e68492a461a56
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-13 18:14:02 +0000

    README updated

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-function getSavedValue(key: string, initialValue: string): string | undefined {
+function getSavedValue(key: string, initialValue: string | (() => void)) {
    const savedValue = localStorage.getItem(key)
-   return savedValue ? JSON.parse(savedValue) : initialValue
+   if (savedValue) return JSON.parse(savedValue)
+   if (initialValue instanceof Function) return initialValue()
+   return initialValue
 }
 
 export function useLocalStorage(key: string, initialValue: string) {
-   const [value, setValue] = useState(initialValue)
-   const savedValue = getSavedValue(key, initialValue)
+   const [value, setValue] = useState(() => getSavedValue(key, initialValue))
+
+   useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value))
+   }, [value, key])
 
    return [value, setValue]
 }
+
+// instanceof vs typeof -
+// useState() func -
