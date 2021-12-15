4189b4f18ade2a69f82f1f4f551108170bbdb935
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-15 16:56:15 +0000

    Stories page done

--- src/styles/Button.styled.tsx
+++ src/styles/Button.styled.tsx
@@ -14,10 +14,11 @@ export const TimerButton = styled(ButtonReset).attrs<ButtonProps>(
       active,
    })
 )<ButtonProps>`
-   padding: 2em 1.5em;
+   padding: 1.3em 1.7em;
    border-radius: 50px;
    color: ${({ theme: { baseStyles }, active }) =>
       active ? baseStyles.bgSd : baseStyles.lightGrey};
    background-color: ${({ theme: { currentTheme }, active }) =>
       active ? currentTheme.accent : 'transparent'};
+   font-weight: bold;
 `
