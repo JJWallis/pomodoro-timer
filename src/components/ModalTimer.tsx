b7b6e4141d48806d7d9070414056941bec33c763
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-15 10:02:14 +0000

    Controlled number input ip

--- src/components/ModalTimer.tsx
+++ src/components/ModalTimer.tsx
@@ -10,6 +10,9 @@ const ModalTimer: FC<Props> = ({ label }) => {
    const updateState = (newState: number) => {
       if (newState > 0 && newState !== 0) setState(newState)
       // dispatch an action to update App state - match dispatch names with label prop
+      // only once apply btn hit at bottom of modal
+      // model state in obj in parent/reducer - custom hook to update (same method for each input)
+      // diff state props to each input
    }
 
    return (
