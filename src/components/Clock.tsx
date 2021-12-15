feb7e2d198f349f93a2e9b791a4e1ac2983d1b40
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-15 18:43:31 +0000

    Stories page done

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react'
 import { ClockButton } from '../styles/Button.styled'
+import { TimerTitle } from '../styles/Title.styled'
 
 const Clock: FC = () => {
-   return <ClockButton></ClockButton>
+   return (
+      <ClockButton>
+         <TimerTitle>0:00</TimerTitle>
+         <TimerTitle subTitle>pause</TimerTitle>
+      </ClockButton>
+   )
 }
 
 export default Clock
--- src/styles/Title.styled.tsx
+++ src/styles/Title.styled.tsx
@@ -1,6 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface Props {
+   subTitle?: boolean
+}
 
 export const Title = styled.h1`
    font-size: 1.5rem;
    color: ${({ theme: { baseStyles } }) => baseStyles.lightGrey};
+`
+
+export const TimerTitle = styled.h2<Props>`
+   font-size: 4rem;
+   color: ${({ theme: { baseStyles } }) => baseStyles.softWhite};
+   ${({ subTitle }) =>
+      subTitle &&
+      css`
+         font-size: 1.5rem;
+         text-transform: uppercase;
+      `}
 `
