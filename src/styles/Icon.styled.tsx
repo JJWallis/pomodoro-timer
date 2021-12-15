1a0e90e3ecd05dcee3c1e1d7117b59cd24e60215
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-15 18:01:17 +0000

    Stories page done

--- src/styles/Icon.styled.tsx
+++ src/styles/Icon.styled.tsx
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const Cog = styled.svg``
+export const Cog = styled.svg`
+   width: 50px;
+   height: 50px;
+   opacity: 0.6;
+   fill: #ced6fa;
+   transition: fill 0.2s ease-in-out;
+   &:hover {
+      fill: ${({ theme: { currentTheme } }) => currentTheme.accent};
+   }
+`
