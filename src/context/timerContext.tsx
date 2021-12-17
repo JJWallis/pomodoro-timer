c276067ce469305a1b33ec56913dce8f17740a14
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-17 22:51:04 +0000

    Friday last

--- src/context/timerContext.tsx
+++ src/context/timerContext.tsx
@@ -5,7 +5,6 @@ import { TimerContextData } from './timerContext.interface'
 export const TimerContext = createContext<TimerContextData | null>(null)
 
 export const TimerContextProvider = ({ children }: { children: ReactNode }) => {
-   // split where logic is kept - part in custom hook + here
    return (
       <TimerContext.Provider value={useTimer()}>
          {children}
