dca8bedf06325cfd14bcca698853b99c6b792732
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-17 20:27:53 +0000

    Timer custom hook ip

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -3,7 +3,8 @@ import { useState, useCallback } from 'react'
 export function useTimer(startTime: number) {
    const [timerLength, setTimerLength] = useState(startTime ?? 1.0)
    const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null)
-   //    reducer - both change at same time
+   const [isRunning, setIsRunning] = useState(false)
+   //    reducer - multiple changing at same time
 
    const startTimer = useCallback(() => {
       const time = setInterval(
