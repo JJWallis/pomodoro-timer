ce0a99b7b0e9be17a58d0eb2e73c2bdb057dba12
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-19 18:01:25 +0000

    Circle meter ip

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,11 +3,11 @@ import { ProgressMeter } from '../containers/ProgressBar.styled'
 import { useTimerContext } from '../hooks/useTimerContext'
 
 const ProgressBar: FC = () => {
-   const [progressWidth, setProgressWidth] = useState(0)
+   const [progressWidth, setProgressWidth] = useState(50)
    const { timerLength } = useTimerContext()
 
    useEffect(() => {
-      setProgressWidth((prev) => prev + 5)
+      setProgressWidth((prev) => prev + 1)
       if (timerLength === 0) setProgressWidth(0)
    }, [timerLength])
 
--- src/containers/ProgressBar.styled.tsx
+++ src/containers/ProgressBar.styled.tsx
@@ -6,13 +6,30 @@ interface Props {
 }
 
 export const ProgressMeter = styled.div<Props>`
-   position: absolute;
-   width: calc(${({ progressAmount }) => progressAmount} * 1%);
-   height: 10px;
+   position: relative;
+   width: 100%;
+   height: 100%;
    min-width: 10px;
    max-width: 100%;
    /* kyle does calc(100% - offset value to fit in parent bar) */
-   border-radius: 5px;
+   border-radius: 50%;
    inset: 0;
    background-color: ${({ theme: { currentTheme } }) => currentTheme.accent};
+   background: conic-gradient(
+      ${({ theme: { currentTheme } }) => currentTheme.accent}
+         ${({ progressAmount }) => progressAmount}%,
+      ${({ theme: { baseStyles } }) => baseStyles.bgSd} 50%
+   );
+   &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 84%;
+      height: 84%;
+      border-radius: 50%;
+      background-color: ${({ theme: { baseStyles } }) => baseStyles.bgSd};
+   }
 `
