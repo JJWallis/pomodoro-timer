c53b7e95f87266f8cb1f306892df27148602521f
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-19 13:52:09 +0000

    prevtimer ref ip

--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -7,7 +7,7 @@ import { TimerButton } from '../styles/Button.styled'
 const Timers: FC = () => {
    const [active, setActive] = useState('pomodoro')
    const { state } = useModalContext()
-   const { dispatch, endTimer } = useTimerContext()
+   const { dispatch, endTimer, prevTimer } = useTimerContext()
 
    const handleClick = (desiredTimer: string) => {
       if (active !== desiredTimer) {
@@ -17,9 +17,9 @@ const Timers: FC = () => {
             type: 'SET_INITIAL_TIMER_LENGTH',
             amount: state[desiredTimer as keyof typeof state],
          })
+         prevTimer.current = { timer: desiredTimer, amount: 0 }
       }
 
-      // updates timer prop with key (desiredTimer) everytime
       // pomodoroAmount only updated if desiredTimer is not pomodoro && timer prop was pomodoro (will be by default)
       // code order - update timer prop to new key after this
       // setting when returning (desiredTimer === 'pomodoro') - if pomodoroAmount prop is truthy (use that for dispatch amount)
