b2783a89477fd92044e5d5b4a87afa368f5995ae
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-19 11:10:13 +0000

    Timers ip

--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,17 +1,21 @@
 import React, { FC, useState } from 'react'
 import { TimersContainer } from '../containers/TimersContainer.styled'
 import { useModalContext } from '../hooks/useModalContext'
+import { useTimerContext } from '../hooks/useTimerContext'
 import { TimerButton } from '../styles/Button.styled'
-
-// type TimerStates = 'shortbreak' | 'longbreak' | 'pomodoro'
 
 const Timers: FC = () => {
    const [active, setActive] = useState('pomodoro')
    const { state } = useModalContext()
+   const { dispatch, endTimer } = useTimerContext()
 
-   const handleClick = (type: string) => {
-      setActive(type)
-      // dispatch
+   const handleClick = (desiredTimer: string) => {
+      setActive(desiredTimer)
+      endTimer()
+      dispatch({
+         type: 'SET_INITIAL_TIMER_LENGTH',
+         amount: state[desiredTimer as keyof typeof state],
+      })
    }
 
    return (
