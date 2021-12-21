ac643529fcff3e3f6b7041841a684fb60746a556
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-21 18:20:55 +0000

    Refactored

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -15,13 +15,11 @@ const ModalHeader: FC<Props> = ({ setIsModalToggled }) => {
          <FlexContainerModal header>
             <h3>Settings</h3>
             <Button cross type="button" onClick={setIsModalToggled}>
-               <Close>
-                  <svg xmlns="http://www.w3.org/2000/svg">
-                     <path
-                        fillRule="evenodd"
-                        d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"
-                     />
-                  </svg>
+               <Close xmlns="http://www.w3.org/2000/svg">
+                  <path
+                     fillRule="evenodd"
+                     d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"
+                  />
                </Close>
             </Button>
          </FlexContainerModal>
