f8a48057dddc8e57f6132a5869d6d0807317f13e
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-27 14:24:38 +0000

    CSS refactoring ip

--- src/containers/TimersContainer.styled.tsx
+++ src/containers/TimersContainer.styled.tsx
@@ -7,7 +7,11 @@ export const TimersContainer = styled.div`
    width: 100%;
    max-width: 440px;
    padding: 0.4rem 0.5rem;
-   margin-bottom: 2.5rem;
+   margin-bottom: 1rem;
    border-radius: 50px;
    background-color: ${({ theme: { baseStyles } }) => baseStyles.bgSd};
+   @media (min-width: ${({ theme: { baseStyles } }) =>
+         baseStyles.mediaBreakpoint}) {
+      margin-bottom: 2rem;
+   }
 `
