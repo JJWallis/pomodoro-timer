76cf49218510ece7b0e7634e166721f9d9f1acea
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-29 16:27:16 +0000

    SVGS ip

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -14,9 +14,9 @@ const ModalHeader: FC<Props> = ({ setIsModalToggled }) => {
    return (
       <ModalSection header>
          <FlexContainerModal as="header" header>
-            <ModalTitle>Settings</ModalTitle>
+            <ModalTitle as="h2">Settings</ModalTitle>
             <Button cross onClick={setIsModalToggled}>
-               <Close xmlns="http://www.w3.org/2000/svg" width="18" height="17">
+               <Close xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 17">
                   <path
                      fillRule="evenodd"
                      d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"
