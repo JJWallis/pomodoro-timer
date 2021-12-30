38ae54d295e210265707d05922e9bb4a4cdb730d
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-30 11:41:49 +0000

    ModalColors refactored

--- src/components/ModalColors.tsx
+++ src/components/ModalColors.tsx
@@ -1,23 +1,20 @@
 import React, { FC, memo, useContext } from 'react'
 import { ThemeContext } from 'styled-components'
-import { useRadioButton } from '../hooks/useRadioButton'
 import ModalSection from './ModalSection'
-import { FlexContainerModal } from '../containers/Container.styled'
-import { ModalTitle } from '../styles/Title.styled'
+import ModalThemeSection from './ModalThemeSection'
 
 const ModalColors: FC = () => {
    const { currentTheme, updateTheme } = useContext(ThemeContext)
    const colors = ['redTheme', 'blueTheme', 'purpleTheme']
-   const produceRadioButtons = useRadioButton(currentTheme, colors, updateTheme)
 
    return (
       <ModalSection colors>
-         <FlexContainerModal>
-            <ModalTitle subTitle>Color</ModalTitle>
-            <FlexContainerModal radioButtons>
-               {produceRadioButtons}
-            </FlexContainerModal>
-         </FlexContainerModal>
+         <ModalThemeSection
+            title="Color"
+            currentTheme={currentTheme}
+            themes={colors}
+            updateTheme={updateTheme}
+         />
       </ModalSection>
    )
 }
