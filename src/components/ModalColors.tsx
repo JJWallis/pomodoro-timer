ed2a2ed1327535a08602b5a2b41359ac894b1b4e
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-30 15:52:51 +0000

    Partial type added

--- src/components/ModalColors.tsx
+++ src/components/ModalColors.tsx
@@ -2,20 +2,22 @@ import React, { FC, memo, useContext } from 'react'
 import { ThemeContext } from 'styled-components'
 import ModalSection from './ModalSection'
 import ModalThemeSection from './ModalThemeSection'
-import { Theme } from '../styles/Theme'
+import { Theme, ColorThemes } from '../styles/Theme'
 
 const ModalColors: FC = () => {
    const { currentTheme, updateTheme } = useContext(ThemeContext)
-   const colors = Object.values(Theme)
-      .splice(1, 3)
-      .map((color: any) => color.identifier)
+   const produceColors = (color: Partial<ColorThemes>) => {
+      return Object.values(color)
+         .splice(1, 3)
+         .map((color) => color.identifier)
+   }
 
    return (
       <ModalSection colors>
          <ModalThemeSection
             title="Color"
             currentTheme={currentTheme}
-            themes={colors}
+            themes={produceColors(Theme)}
             updateTheme={updateTheme}
          />
       </ModalSection>
