75b954564267801a906b74a53c7c77bbd4661f18
JJWallis <josh.jamesonwallis@gmail.com>
2021-12-31 10:27:16 +0000

    Custom hook ip

--- src/hooks/useClickOutside.tsx
+++ src/hooks/useClickOutside.tsx
@@ -1,27 +1,30 @@
 import React, { useRef, useEffect } from 'react'
 
 type UseClickOutside = <T>(
-   elRef: React.MutableRefObject<any>,
+   elRef: React.RefObject<HTMLElement>,
    callback: T
 ) => void
 
 export const useCLickOutside: UseClickOutside = (elRef, callback) => {
-   const callbackRef = useRef(callback)
+   const callbackRef = useRef<typeof callback>()
+   callbackRef.current = callback
 
    useEffect(() => {
       const handleClickOutside = (e: MouseEvent) => {
          if (
-            elRef?.current?.contains(e.target as Node) &&
-            callbackRef.current &&
+            !elRef?.current?.contains(e.target as Node) &&
             callbackRef.current instanceof Function
-         ) {
+         )
             callbackRef.current(e)
-         }
       }
 
       document.addEventListener('click', handleClickOutside, true)
       return () => {
          document.removeEventListener('click', handleClickOutside, true)
       }
-   }, [elRef, callback])
+   }, [elRef, callbackRef])
 }
+
+// instead of referencing callback in useEffect, we can use a ref to reference it
+// else infinite loop | ref never changes (only instigated on original hook call)
+// but the value it holds will change (dynamic)
