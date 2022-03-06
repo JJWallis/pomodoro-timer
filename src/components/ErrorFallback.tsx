5971b6df70afde816e4068850e36a9d5fa05e1a7
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-06 09:53:36 +0000

    error message styles done

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react'
 import { FallbackProps } from 'react-error-boundary'
+import ErrorMsg from '../styles/ErrorMsg.styled'
 
 const ErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
    return (
       <div role="alert">
-         <p>Something went wrong:</p>
-         <pre>{error.message}</pre>
+         <ErrorMsg>
+            Something went wrong: <br />
+            {error.message}
+         </ErrorMsg>
          <button onClick={resetErrorBoundary}>Try again</button>
       </div>
    )
--- src/styles/ErrorMsg.styled.tsx
+++ src/styles/ErrorMsg.styled.tsx
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
-export default styled.p``
+export default styled.p`
+   color: ${({ theme }) => theme.baseStyles.lightGrey};
+   font-size: 2rem;
+   text-align: center;
+`
