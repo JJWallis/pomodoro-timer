b146beb5a89d431bc3ae3195efb19351ea44636d
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-06 09:34:55 +0000

    error fallback component setup

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -2,7 +2,13 @@ import React, { FC } from 'react'
 import { FallbackProps } from 'react-error-boundary'
 
 const ErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
-   return <div></div>
+   return (
+      <div>
+         <p>Whoops,</p>
+         <pre>{error.message || 'something went wrong'}</pre>
+         <button onClick={resetErrorBoundary}>Please try again</button>
+      </div>
+   )
 }
 
 export default ErrorFallback
